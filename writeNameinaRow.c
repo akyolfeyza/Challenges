e8679b1b55fca1249630a5b3bebd60840bd09f5b
akyolfeyza <120449870+akyolfeyza@users.noreply.github.com>
2022-12-13 11:20:45 +0300

    FeyzaAkyol.c
    @themanoftalent

--- writeNameinaRow.c
+++ writeNameinaRow.c
@@ -1,17 +1,23 @@
 // Online C compiler to run C program online
 #include <stdio.h>
+
 int main(){
- int i,j,k,n; 
- printf("Enter the number of the row :");
- scanf("%d",&n);
- 
- for(i=0;i<n;i++){
-     for(j=0;i<n;i++){
-     
-     
- }
-     printf("Akif")
- }
- 
+int i,j,k,n;
+printf("Enter tne number of rows :");
+scanf("%d", &n);
+
+for(i=1; i<=n; i++)
+{
+    for(j=1; j<=i; j++)
+    {
+        printf("Akif");
+    }
+    for(k=1; k<=n-1; k++)
+    {
+        printf("  ");
+    }
+    printf("\n");
+}    
     
+    return 0;
 }
